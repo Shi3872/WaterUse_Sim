166e2426d6bfce41bfc54c277750e5692a383b4d
Sushi3872 <145415525+Shi3872@users.noreply.github.com>
2025-09-13 09:56:48 -0400

    changes in water values

--- Simulation/main.py
+++ Simulation/main.py
@@ -9,12 +9,12 @@ def create_test_inflows(case):
         return np.array([12000.0] * 200)  # consistently low inflow
     elif case == "3":
         return np.array([
-        38441.4, 29340.4, 19380.8, 29041.0, 36600.0, 33140.8, 26109.6, 21661.2,
-        31198.8, 42920.0, 29900.0, 27120.4, 31197.6, 39683.6, 26468.4, 34140.0,
-        33420.8, 27146.0, 43128.0, 30960.0, 30960.0, 29366.0, 33210.0, 45460.0,
-        38540.0, 30481.2, 18012.4, 31740.0, 26412.0, 27720.0, 38376.0, 46333.8,
-        33700.0, 39010.0, 31260.0, 29880.0, 18200.0, 28600.0, 32800.0, 45500.0,
-        31170.0, 42660.0, 30060.0, 33060.0, 38520.0, 38640.0, 14360.0, 34220.0,
+        22000.0, 27010.0, 38441.4, 25340.4, 18380.8, 16041.0, 22600.0, 33140.8, 26109.6, 20661.2,
+        14198.8, 22920.0, 17021.0, 29900.0, 31120.4, 35197.6, 26683.6, 22468.4, 18140.0,
+        23420.8, 27146.0, 43128.0, 33960.0, 30960.0, 26366.0, 22210.0, 19460.0,
+        28540.0, 33481.2, 26012.4, 24740.0, 20412.0, 27720.0, 38376.0, 46333.8,
+        32700.0, 25260.0, 18200.0, 28600.0, 32800.0, 45500.0,
+        31170.0, 20301.0, 14360.0, 34220.0,
     ]* 100)
     else:
         return np.random.uniform(5000, 40000, 200)  # default random inflow
@@ -56,6 +56,48 @@ def run_multiple_sims(memory_strength=0, centralized=False, fishing_enabled=Fals
 if __name__ == "__main__":
     inflows = create_test_inflows("1")  # change case here
 
+
+    #fish plot
+    deltas = np.linspace(0, 1, 11)
+
+    central_adults, central_larvae = [], []
+    dec_nf_adults, dec_nf_larvae = [], []
+    dec_f_adults, dec_f_larvae, dec_f_catch = [], [], []
+
+    for d in deltas:
+        print("\n-----------------------Centralized no fishing-----------------")
+        _, a, l, _, _ = run_multiple_sims(d, centralized=True, fishing_enabled=False)
+        central_adults.append(a)
+        central_larvae.append(l)
+
+        print("'\n -----------------------Decentralized with fishing-----------------")
+        _, a, l, c, _ = run_multiple_sims(d, centralized=False, fishing_enabled=True)
+        dec_f_adults.append(a)
+        dec_f_larvae.append(l)
+        dec_f_catch.append(c)
+
+        print("\n -----------------------Decentralized no fishing-----------------")
+        _, a, l, _, _ = run_multiple_sims(d, centralized=False, fishing_enabled=False)
+        dec_nf_adults.append(a)
+        dec_nf_larvae.append(l)
+
+    fish_plot(deltas, central_adults, central_larvae, dec_nf_adults, dec_nf_larvae, dec_f_adults, dec_f_larvae, dec_f_catch)
+    
+    #farmer 9 plot
+    deltas = [0, 1]
+    results_by_delta = {}
+
+    for d in deltas:
+        _, _, _, _, farmer9_returns = run_multiple_sims(d, centralized=False, fishing_enabled=True)
+        results_by_delta[d] = farmer9_returns 
+        
+    farmer_returns_plot(results_by_delta)
+    
+    '''
+    sim_delta0 = run_multiple_sims(memory_strength=0, centralized=True, return_sim=True)
+    sim_delta1 = run_multiple_sims(memory_strength=1, centralized=True, return_sim=True)
+    water_plot(sim_delta0, sim_delta1)
+
     # box plot
     results_delta0 = run_multiple_sims(memory_strength=0, centralized= False)
     results_delta1 = run_multiple_sims(memory_strength=1)
@@ -73,11 +115,11 @@ if __name__ == "__main__":
     box_plot_dv(results_decentralized)
     
     # box plot
+    results_complex_cpr = run_multiple_sims(centralized=True, use_cpr_game=True)
     results_delta0 = run_multiple_sims(memory_strength=0, centralized= True)
     results_delta1 = run_multiple_sims(memory_strength=1, centralized=True)
-    results_complex_cpr = run_multiple_sims(centralized=True, use_cpr_game=True)
-
-
+    
+    
     results_centralized = {
     "Heuristics delta 0": results_delta0[0],
     "Heuristics delta 1": results_delta1[0],
@@ -85,3 +127,4 @@ if __name__ == "__main__":
     }
 
     box_plot_cv(results_centralized)
+    '''
